0064261d48f38a4371949c4b9a09f5700357ed02
Marcel <Marcel@Marcel_Windows8>
2017-08-13 23:15:53 +0200

    Neues Projekt - Anzeige und Verwaltung des Speicherplatzes der Laufwerke und Partitionen des PCs

---
+++ src/javadateisystemverwaltung/JAVALaufwerbelegungsErmittler.java
@@ -0,0 +1,19 @@
+package javadateisystemverwaltung;
+
+/**
+ *
+ * @author Marcel Huber; letzte Änderung 13.08.2017
+ */
+public class JAVALaufwerbelegungsErmittler {
+
+    public static void main(String[] args) {
+        JAVALaufwerbelegungsErmittler dummyObject
+                = new JAVALaufwerbelegungsErmittler();
+        dummyObject.showLaufwerksbelegung();
+    }
+
+    private void showLaufwerksbelegung() {
+      
+    }
+
+}
